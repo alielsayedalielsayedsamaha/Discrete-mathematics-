b09c01dc707dab586a87ae4862ecc24b95744bd0
alielsayedalielsayedsamaha <121573819+alielsayedalielsayedsamaha@users.noreply.github.com>
2022-12-29 19:46:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace الأعداد_المثاليه_بين_رقمين
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, num, i;
+            Console.WriteLine("MIN:");
+            n1= int.Parse(Console.ReadLine());
+            Console.WriteLine("MAX:");
+            n2 = int.Parse(Console.ReadLine());
+            for (num = n1; num < n2; num++)
+            {
+                decimal sum = 0;
+                for (i = 1; i < num; i++)
+                {
+                    if (num % i == 0)
+                        sum = sum + i;
+                   
+                }
+                if (sum == num)
+                {
+                    Console.WriteLine(num);
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ program .th
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, v, j;
+            Console.WriteLine("MAX:");
+            v =int.Parse(Console.ReadLine());
+            Console.WriteLine("MIN:");
+            for (i = int.Parse(Console.ReadLine()); i < v; i++)
+            {
+                for (j=2;j<=i;j++)
+                    if (i%j == 0)
+                    {
+
+                        break;
+                    }
+                if(i==j)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
